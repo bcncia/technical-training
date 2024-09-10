084886424f001ef60dab7de7c6b29268a98b9ec7
Christian Infante <cinfante@bcncons.com>
2024-09-10 15:10:15 +0000

    compute 6

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -12,3 +12,5 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id= fields.Many2one("res.partner", string="Partner",required=True)
     property_id= fields.Many2one("estate.property", string="Property",required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date()
