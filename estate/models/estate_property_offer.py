6571008ef54ec104d7e50531e5e4a4aabcedc0de
Christian Infante <cinfante@bcncons.com>
2024-09-10 15:28:13 +0000

    compute 10

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -12,10 +12,10 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id= fields.Many2one("res.partner", string="Partner",required=True)
     property_id= fields.Many2one("estate.property", string="Property",required=True)
-    validity = fields.Integer(string="Validity (days)",default=7,compute="_compute_deadline", inverse="_inverse_compute_days")
-    date_deadline = fields.Date(string="Deadline")
+    validity = fields.Integer(string="Validity (days)",default=7)
+    date_deadline = fields.Date(string="Deadline",compute="_compute_deadline", inverse="_inverse_compute_days")
 
-    @api.depends("date_deadline")
+    @api.depends("validity")
     def _compute_deadline(self):
         for record in self:
             if record.create_date:
