710adfd82471e5d405c330c6ee8522c3ef3fec3f
Christian Infante <cinfante@bcncons.com>
2024-09-10 15:19:29 +0000

    compute 7

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -12,5 +12,15 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id= fields.Many2one("res.partner", string="Partner",required=True)
     property_id= fields.Many2one("estate.property", string="Property",required=True)
-    validity = fields.Integer(default=7)
-    date_deadline = fields.Date()
+    validity = fields.Integer(string="Validity (days)",default=7,compute="_compute_deadline", inverse="_inverse_compute_days")
+    date_deadline = fields.Date(string="Deadline")
+
+    @api.depends("date_deadline")
+    def _compute_deadline(self):
+        for record in self:
+            record.date_deadline = record.create_date + record.validity
+
+    def _inverse_compute_days(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date)
+            
